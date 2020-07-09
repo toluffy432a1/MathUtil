f3ef9098f0bbbab989a41fc5fd24c12d7616a818
toluffy432a1 <toluffy432a1@gmail.com>
2020-07-09 13:38:01 +0700

    1st: first upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# Welcome to Phuc repository
+This repository contains the source code of a simple Java app. It is used to demonstrate the CI - Continuous Integration process, a part of Introduction to Software Engineering course at FPT University HCMC campus
+
+## You will find in this repo: 
+* The MathUtil class (consists of multiple Math methods)
+* Unit Testing by JUnit (for TDD technic)
+* Build configuration file using for Jenkins
+
+## Contact me via:
+1. [Github](https://github.com/toluffy432a1)
+2. [Facebook](https://facebook.com/chop.chep)
+
+### © 2020 by Phuc
---
+++ src/phuc/util/MathUtil.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phuc.util;
+
+/**
+ *
+ * @author ASUS
+ */
+//day la class chứa các hàm tiện ích xài chung cho  các nơi
+//chứa các hàm tính toán để bổ trợ cho các class khác
+//do là đồ hỗ trợ, xài chung, nên chúng là STATIC
+public class MathUtil {
+
+    //hàm tính n! = 1.2.3.....n, vì tăng kết quả rất nhanh, sớm tràn
+    //int chỉ tối đã 2 tỷ 1, phải để long cho đỡ bị tràn kết quả
+    //mình chỉ nên tính n<= 15 vì 20! to lắm
+    public static long computeFactorial(int n) {
+        
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
